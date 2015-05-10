09ab443380da9c6ade8f139a2f0d3ec6e080f36a
bob walker <bob@randomness.org.uk>
2015-05-10 12:25:34 +0000

    Add File::Spec, also chmod u+x

--- install-perl-modules.sh
+++ install-perl-modules.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # install perl modules needed by pubology.co.uk
-cpanm CGI CGI::Carp Config::Tiny File::Copy Template Text::CSV::Simple HTML::TokeParser HTML::Entities Class::Accessor Geo::Coordinates::OSTN02 Geo::Coordinates::OSGB Flickr::API2 Template::Plugin::JSON
+cpanm CGI CGI::Carp Config::Tiny File::Copy Template Text::CSV::Simple HTML::TokeParser HTML::Entities Class::Accessor Geo::Coordinates::OSTN02 Geo::Coordinates::OSGB Flickr::API2 Template::Plugin::JSON File::Spec
